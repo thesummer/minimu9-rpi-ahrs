b039244e8e6382d7ce01140a6db2f384c3b0d9cc
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-09-05 23:41:53 +0200

    Added class files and with function declarations for ITG3200-gyro

---
+++ itg3200.cpp
@@ -0,0 +1,23 @@
+#include "itg3200.h"
+
+ITG3200::ITG3200(const char * i2cDeviceName)
+{
+
+}
+
+void ITG3200::writeReg(uint8_t reg, uint8_t value)
+{
+}
+
+uint8_t ITG3200::readReg(uint8_t reg)
+{
+}
+
+void ITG3200::read()
+{
+}
+
+void ITG3200::detectAddress()
+{
+}
+
---
+++ itg3200.h
@@ -0,0 +1,10 @@
+#ifndef ITG3200_H
+#define ITG3200_H
+
+class ITG3200
+{
+public:
+    ITG3200();
+};
+
+#endif // ITG3200_H
